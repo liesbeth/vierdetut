e2d0bcf144af1b6a15e8158a952f635162de9c22
liesbeth <liesbethvanderplas@gmail.com>
2011-01-15 16:11:49 +0100

    Initial commit van blz. 27

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vierdetut::Application.config.secret_token = '15214c954b0ab2d9ccafb41139ca0e24128e85b746d55c6f3d332ef1bc32ba207365973382ae47d73f8b641338abda89eb529ae7c054c4b089d47070a05cb41d'
